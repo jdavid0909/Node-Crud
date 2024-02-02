3becc2842ba44fb8430edcbdced8875bb70c106e
jdavid0909 <jdavid198.jdr@gmail.com>
2024-02-02 09:57:36 -0600

    crud basico con node

---
+++ models/server.js
@@ -0,0 +1,61 @@
+
+const express = require('express')
+var cors = require('cors');
+const connectionDb = require('../database/config');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port= process.env.PORT;
+
+
+
+        this.usuariosRoutePath = '/api/usuarios'
+
+        //Conecion ala bd
+
+        this.conncectarDB();
+
+
+
+        // Middewalares : funciones que van añadirle otra funcionalidad al web server 
+
+        this.middlewares()
+
+        //Rutas de mi app
+        this.routes();
+    }
+
+    async conncectarDB(){
+        await connectionDb();
+    }
+
+    routes() {
+
+        this.app.use(this.usuariosRoutePath, require('../routes/user'))
+
+    }
+
+    listen(){
+        this.app.listen(this.port,()=>{
+
+            console.log(process.env.PORT);
+        
+        })
+    }
+
+    middlewares(){
+
+        // CORS
+        this.app.use(cors());
+
+        //Parso y Lectura del body
+
+        this.app.use(express.json());
+
+        //directorio publico
+        this.app.use(express.static('public'))    
+    }
+}
+
+module.exports = Server
